8d6c00d19f75fff12b1bd48040a1dfec8209f6eb
freelancersazal <freelancersazal@gmail.com>
2024-05-26 13:10:03 +0600

    home page 2 added

--- js/main.js
+++ js/main.js
@@ -2,21 +2,33 @@ $(function () {
 
     "use strict";
 
-
-
-    //=========MENU FIX JS=========   
-    if ($('.wsus__main_menu').offset() != undefined) {
-        var navoff = $('.wsus__main_menu').offset().top;
-        $(window).scroll(function () {
-            var scrolling = $(this).scrollTop();
-
-            if (scrolling > navoff) {
-                $('.wsus__main_menu').addClass('menu_fix');
-            } else {
-                $('.wsus__main_menu').removeClass('menu_fix');
-            }
-        });
+    //=======MENU FIX======
+    if ($(window).scrollTop() > 1) {
+        if ($('.wsus__main_menu').offset() != undefined) {
+            $('.wsus__main_menu').addClass('menu_fix');
+        }
+    } else {
+        if ($('.wsus__main_menu').offset() != undefined) {
+            $('.wsus__main_menu').removeClass('menu_fix');
+        }
     }
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1) {
+
+            if ($('.wsus__main_menu').offset() != undefined) {
+                // check if menu_if class is already added
+                if (!$('.wsus__main_menu').hasClass("menu_fix")) {
+                    $('.wsus__main_menu').addClass("menu_fix");
+                }
+            }
+        }
+        else {
+            if ($('.wsus__main_menu').offset() != undefined) {
+                $('.wsus__main_menu').removeClass("menu_fix");
+            }
+        }
+    });
 
 
 
@@ -30,8 +42,7 @@ $(function () {
     $('.counter').countUp();
 
 
-
-//*==========TESTIMONIAL SLIDER========= 
+    //*==========TESTIMONIAL SLIDER========= 
     $('.testi_slider').slick({
         slidesToShow: 2,
         slidesToScroll: 1,
@@ -49,7 +60,7 @@ $(function () {
                     dots: true,
                     arrows: false,
                 }
-        },
+            },
             {
                 breakpoint: 992,
                 settings: {
@@ -58,7 +69,7 @@ $(function () {
                     dots: true,
                     arrows: false,
                 }
-        },
+            },
             {
                 breakpoint: 768,
                 settings: {
@@ -67,7 +78,7 @@ $(function () {
                     dots: true,
                     arrows: false,
                 }
-        },
+            },
             {
                 breakpoint: 576,
                 settings: {
@@ -76,9 +87,9 @@ $(function () {
                     dots: true,
                     arrows: false,
                 }
-        }
-       ]
-    }); 
+            }
+        ]
+    });
 
 
 
@@ -126,45 +137,156 @@ $(function () {
         $('input[type="file"]').imageuploadify();
     })
 
-    
-    
-    
-//*==========DASHBOARD MENU==========  
-    
-$('.wsus__menu_icon').on('click', function(){
-    $('.wsus__dashboard_side_bar').addClass('.menu_show');
-});
 
-     
-$('.wsus__close_icon').on('click', function(){
-    $('.wsus__dashboard_side_bar').removeClass('.menu_show');
-});
-    
-    
 
-$('.wsus__menu_icon').on('click', function(){
-    $('.wsus__close_icon').addClass('.show_close_icon');
-});
-    
-$('.wsus__close_icon').on('click', function(){
-    $('.wsus__close_icon').removeClass('.show_close_icon');
-});
-    
-    
-    
-    
-//*==========SMALL DEVICE PROPERTY FILTER==========  
-$('.wsus__filter_icon').on('click', function(){
-    $('.minus_2').toggleClass('.icon_roatet');
-});
-    
-$('.wsus__filter_icon').on('click', function(){
-    $('.wsus__filter_body').toggleClass('.show_filter');
-});
-    
-    
-    
-    
+
+    //*==========DASHBOARD MENU==========  
+
+    $('.wsus__menu_icon').on('click', function () {
+        $('.wsus__dashboard_side_bar').addClass('.menu_show');
+    });
+
+
+    $('.wsus__close_icon').on('click', function () {
+        $('.wsus__dashboard_side_bar').removeClass('.menu_show');
+    });
+
+
+
+    $('.wsus__menu_icon').on('click', function () {
+        $('.wsus__close_icon').addClass('.show_close_icon');
+    });
+
+    $('.wsus__close_icon').on('click', function () {
+        $('.wsus__close_icon').removeClass('.show_close_icon');
+    });
+
+
+    //*==========SMALL DEVICE PROPERTY FILTER==========  
+    $('.wsus__filter_icon').on('click', function () {
+        $('.minus_2').toggleClass('.icon_roatet');
+    });
+
+    $('.wsus__filter_icon').on('click', function () {
+        $('.wsus__filter_body').toggleClass('.show_filter');
+    });
+
+
+
+
+
+
+
+
+
+
+    // Category 2 slider
+    $('.category_2_slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: false,
+        arrows: false,
+
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                }
+            }
+        ]
+    });
+
+
+    // testimonial 2 slider
+    $('.testimonial_slider_2').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: false,
+        arrows: true,
+        nextArrow: '<i class="far fa-arrow-right nextArrow"></i>',
+        prevArrow: '<i class="far fa-arrow-left prevArrow"></i>',
+
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows: false,
+                }
+            }
+        ]
+    });
+
+
+
+
+
+
+
 
 
 });
